5cb77be8d44588c0c081b95f07bdbb714a5684a1
atisfix <56512862+atisfix@users.noreply.github.com>
2026-02-15 00:29:53 +0100

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CarbonFlow from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CarbonFlow />
+  </React.StrictMode>
+)
