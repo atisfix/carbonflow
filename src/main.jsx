cc92970f00b5fa06255fca8c90d0cd4d3d9dd803
atisfix <56512862+atisfix@users.noreply.github.com>
2026-02-15 23:40:12 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CarbonFlow from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CarbonFlow />
-  </React.StrictMode>
-)
